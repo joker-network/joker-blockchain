94c8d0587f2da809cf6af990ebb0de0cf94ab84d
jacksonste83 <jacksonste1983@gmail.com>
2022-03-02 08:06:55 +0800

    fix bug

--- build_scripts/installer-version.py
+++ build_scripts/installer-version.py
@@ -13,9 +13,9 @@ def main():
     os.environ["SCM_VERSION"] = scm_full_version
 
     left_full_version = scm_full_version.split("+")
-    version = left_full_version[0].split(".")
-    scm_major_version = version[0]
-    scm_minor_version = version[1]
+    version = left_full_version[1].split(".")
+    scm_major_version = version[2]
+    scm_minor_version = version[3]
 
     if len(version) == 3:  # If the length of the version array is more than 2
         patch_release_number = version[2]
